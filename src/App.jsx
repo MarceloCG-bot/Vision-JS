318af834d003c93c0d27882bae70427c350410ab
Caique Matheus <dev.caique@gmail.com>
2021-05-08 23:06:56 -0300

    Modificado estilo dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
-import "./App.css";
+import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import WebcamComponent from "./components/webcam";
 
 function App() {
   return (
     <div className="App">
-      <ul class="nav justify-content-center">
-        <h1>CAPTURE IMAGE</h1>
-      </ul>
+      <header className="nav justify-content-center">
+        <h1 className="my-4 fw-bold text-white">Captura de Imagem</h1>
+      </header>
 
-      <div class="divisao-wrap">
-        <div class="divisao-camera">
-          <div id="camera" />
-          <div class="botao">
+      <section className="container-fluid">
+        <div className="row my-4">
+          <div className="box col-md mx-4 justify-content-between">
             <WebcamComponent />
-            <button onclick="takeSnapshot()" class="btn btn-primary">
-              CAPTURE IMAGE
+            <div className="d-flex justify-content-center">
+              <button onclick="takeSnapshot()" class="btn btn-primary">
+                Capturar imagem
             </button>
+            </div>
+          </div>
+
+          <div className="box col-md">
+            <div className="results" />
+          </div>
+
+          <div className="box col-md mx-4 justify-content-end">
+            <div id="uploadedImage" />
+            <img id="croppedImage" alt="cropping result" />
+            <div className="d-flex justify-content-center">
+              <button class="btn btn-warning" id="cropButton">
+                Salvar seleção
+            </button>
+            </div>
           </div>
         </div>
-        <div class="divisao-capturados">
-          <div id="results" />
-        </div>
-        <div class="croppie">
-          <input type="file" name="fileUpload" id="fileUpload" />
-          <div id="uploadedImage" />
-          <img id="croppedImage" alt="cropping result" />
-          <button class="btn btn-warning aviso" id="cropButton">
-            Save Selection
-          </button>
-        </div>
-      </div>
+      </section>
+
       <script src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js" />
     </div>
   );
